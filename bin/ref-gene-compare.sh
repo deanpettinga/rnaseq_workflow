2274a816838e958807dbe0bc4d5dad46824ca31a
Dean Pettinga <deanpett@gmail.com>
2020-10-12 09:38:29 -0700

    refixed

--- bin/ref-gene-compare.sh
+++ bin/ref-gene-compare.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # get annotation field
-awk '{print $9}' ../../../refs/HanXRQr2.0-SUNRISE-2.1/annotation/HanXRQr2.0-SUNRISE-2.1.gff3 |\
+awk '{print $9}' ../../refs/HanXRQr2.0-SUNRISE-2.1/annotation/HanXRQr2.0-SUNRISE-2.1.gff3 |\
  # get only rows with 'gene' annotation
  grep 'gene:' | awk 'BEGIN {FS = ":"} ; {print $2}' |\
  # extract just the gene name
